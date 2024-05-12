b8705520b5d609e52a04a8b917f196dda34f4318
dytsou <d.y.tsou@gmail.com>
2024-05-12 18:28:18 +0800

    hw5 init

---
+++ hw5/blockchain.cpp
@@ -0,0 +1,17 @@
+#include "cryptlib.h"
+#include "rijndael.h"
+#include "modes.h"
+#include "files.h"
+#include "osrng.h"
+#include "hex.h"
+#include "rsa.h"
+#include <iostream>
+#include <string>
+#define ll unsigned long long
+#define int unsigned long long
+
+
+signed main(int argc, char *argv[]){
+    
+    return 0;
+}
